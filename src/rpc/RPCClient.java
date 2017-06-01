f2e67a6ec8d162964743c60291df65776f2d50bf
zhaoshiqiang <523618033@qq.com>
2017-06-01 08:51:24 +0800

    add dynamic proxy

--- src/rpc/RPCClient.java
+++ src/rpc/RPCClient.java
@@ -1,7 +1,22 @@
 package rpc;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * Created by zhaoshiqiang on 2017/6/1.
  */
 public class RPCClient {
+    public <T> T getProxy(Class<T> cls){
+        return (T) Proxy.newProxyInstance(cls.getClassLoader(),new Class[]{cls},new invoke());
+    }
+
+    private class invoke implements InvocationHandler{
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return null;
+        }
+    }
 }
