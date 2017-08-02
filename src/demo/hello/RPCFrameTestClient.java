ee682e82aecbaa23dfc13362807a8959b549a97f
zhaoshq <523618033@qq.com>
2017-08-02 19:54:45 +0800

    the client connection address, port and arg are assigned by main

--- src/demo/hello/RPCFrameTestClient.java
+++ src/demo/hello/RPCFrameTestClient.java
@@ -1,23 +1,34 @@
 package demo.hello;
 
 import rpc.RPCClient;
+import rpc.RPCException;
 
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Created by zhaoshiqiang on 2017/6/5.
  */
 public class RPCFrameTestClient {
 
-    public static void main(String[] args) throws UnknownHostException, ExecutionException, InterruptedException {
-
-        InetSocketAddress addr = new InetSocketAddress("localhost", 8080);
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.out.println("args: hostname port name");
+            return;
+        }
+        InetSocketAddress addr = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
         IHello hello = new RPCClient<IHello>(addr, IHello.class).getProxy();
-        Object result = hello.hello("zhaoshiqiang",1);
+        Object result = hello.hello(args[2]);
         System.out.println(result);
         System.out.println(hello.hello());
 
+//        RPCClient client = new RPCClient(addr);
+//        client.open();
+//        Method helloMethod = IHello.class.getMethod("hello", String.class);
+//        Future future = client.invoke(helloMethod, new Object[]{"zhaoshiqiang"});
+//        future.get();
     }
 }
