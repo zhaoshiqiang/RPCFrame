a1c51e0cbdacf5a634a79aa836943a478d66d4b4
zhaoshq <523618033@qq.com>
2017-08-02 19:41:44 +0800

    add implement the IContextListener interface

--- src/demo/hello/HelloImpl.java
+++ src/demo/hello/HelloImpl.java
@@ -1,19 +1,20 @@
 package demo.hello;
 
 import server.Context;
+import server.IContextListener;
 import server.ThreadState;
 
 /**
  * Created by zhaoshiqiang on 2017/6/5.
  */
-public class HelloImpl implements IHello {
+public class HelloImpl implements IHello , IContextListener{
     @Override
-    public String hello(String name, int number) {
+    public String hello(String name) {
         Context context = ThreadState.getContext();
         if (context != null){
             context.put("name",name);
         }
-        return "hello " + name + " ! number is " + number;
+        return "hello " + name +" !";
     }
 
     @Override
@@ -22,8 +23,24 @@ public class HelloImpl implements IHello {
         if (context != null){
             return "hello" + context.get("name");
         }else {
-            return null;
+            return "Hello, may I known your name?";
         }
     }
 
+    @Override
+    public void onContextCreate(Context context) {
+        context.put("_start", System.currentTimeMillis());
+    }
+
+    @Override
+    public void onContextDestory(Context context) {
+        long start = (Long) context.get("_start");
+         long end = System.currentTimeMillis();
+         String name = (String) context.get("name");
+         if (name == null) {
+             System.out.println("unknown user stayed here for " + (end-start) + "ms.");
+         } else {
+             System.out.println(name + " stayed here for " + (end-start) + "ms.");
+         }
+    }
 }
