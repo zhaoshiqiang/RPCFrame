cd78183806504e531a71e034754139c5dab518d9
zhaoshq <523618033@qq.com>
2017-08-02 19:43:45 +0800

    the server port is assigned by args

--- src/demo/hello/PRCFrameTestServer.java
+++ src/demo/hello/PRCFrameTestServer.java
@@ -9,11 +9,13 @@ import java.io.IOException;
  * Created by zhaoshiqiang on 2017/6/6.
  */
 public class PRCFrameTestServer {
-    private static final int port = 8080;
-
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        Server server =new Server(port,1,new RequestHandler(new HelloImpl()),3);
+        if (args.length < 1) {
+            System.out.println("args: port");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        Server server =new Server(port,new HelloImpl());
         server.start();
         System.out.println("HelloServer started on port " + port);
         server.join();
